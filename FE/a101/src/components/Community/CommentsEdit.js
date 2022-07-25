0e7a659b5f1af7bf025417cad27ae3f9f6f459e0
kd8317 <kd8317@naver.com>
2022-07-25 13:44:01 +0900

    chore: Reinstall react library and folders

---
+++ FE/a101/src/components/Community/CommentsEdit.js
@@ -0,0 +1,7 @@
+export default function CommentsEdit() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ FE/a101/src/components/Community/Frame.js
@@ -0,0 +1,7 @@
+export default function Frame() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
