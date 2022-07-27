1cca59b8879379b61d48fcf535d1e4ecf82c78bc
junsu <xofkdqkqh@naver.com>
2022-07-27 16:43:04 +0900

    chore : s3성공

--- BE/phorest/src/main/java/a101/phorest/controller/ImageController.java
+++ BE/phorest/src/main/java/a101/phorest/controller/ImageController.java
@@ -1,7 +1,10 @@
 package a101.phorest.controller;
 
+import a101.phorest.S3Uploader;
 import a101.phorest.service.ImageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -11,9 +14,20 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("api")
 public class ImageController {
 
-    @PostMapping("upload/{frameId}")
+    @PostMapping("upload")
     @ResponseBody
     public String updateUserImage(@RequestBody MultipartFile multipartFile) {
         return "success";
+    }
+
+    private final S3Uploader s3Uploader;
+
+    @PostMapping("upload/temp")
+    public ResponseEntity updateUserImage2(@RequestParam("images") MultipartFile multipartFile) {
+        try {
+            String uploadUrl = s3Uploader.uploadFiles(multipartFile, "static");
+            System.out.println(uploadUrl);
+        } catch (Exception e) { return new ResponseEntity(HttpStatus.BAD_REQUEST); }
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
