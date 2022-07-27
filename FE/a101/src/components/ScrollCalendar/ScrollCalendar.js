fb5f274f2b990f6610ea4a87e03e8fd74e84c96b
김보경 <rawoon@kakao.com>
2022-07-27 16:01:00 +0900

    refactor: 스크롤은 되는데 겹치는 날따가 생겼다

--- FE/a101/src/components/ScrollCalendar/ScrollCalendar.js
+++ FE/a101/src/components/ScrollCalendar/ScrollCalendar.js
@@ -11,29 +11,42 @@ const Item = styled(Paper)(({ theme, props }) => ({
   color: theme.palette.text.secondary,
 }));
 
-// 오늘 연월일 구하기
-const today = new Date();
+// 먼저 올해의 년도를 구한다.
+// 그리고 12월을 구해서 각각에 대해서 days 를 저장한다.
+// let month = [{month: 0, days: [220101, 220102....]}, {month: 0, days: [220101, 220102....]}, ...]
+let months = []
+const today = new Date()
 const year = today.getFullYear();
-const month = today.getMonth();
-// 달의 시작일 구하기
-const firstDate = new Date(year, month, 1)
-const firstDateofWeek = firstDate.getDay()
-// 달의 시작 주차의 일요일 날짜
-const firstDateofMonth = new Date(firstDate.getFullYear(), firstDate.getMonth(), firstDate.getDate() - firstDateofWeek)
-// 달의 마지막 일 
-const lastDate = new Date(year, month + 1, 0)
-const lastDateofWeek = lastDate.getDay()
-// 달의 마지막 주차의 토요일 날짜
-const lastDateofMonth = new Date(lastDate.getFullYear(), lastDate.getMonth(), lastDate.getDate() + (6 - lastDateofWeek))
-console.log(lastDateofMonth)
+function calMonth(month) {
+  // 달의 시작일 구하기
+  const firstDate = new Date(year, month, 1)
+  const firstDateofWeek = firstDate.getDay()
+  // 달의 시작 주차의 일요일 날짜
+  const firstDateofMonth = new Date(firstDate.getFullYear(), firstDate.getMonth(), firstDate.getDate() - firstDateofWeek)
+  // 달의 마지막 일 
+  const lastDate = new Date(year, month + 1, 0)
+  const lastDateofWeek = lastDate.getDay()
+  // 달의 마지막 주차의 토요일 날짜
+  const lastDateofMonth = new Date(lastDate.getFullYear(), lastDate.getMonth(), lastDate.getDate() + (6 - lastDateofWeek))
 
-let monthlyDays = []
-let day = firstDateofMonth
-while (day <= lastDateofMonth) {
-  monthlyDays.push(day)
-  day = new Date(day.getFullYear(), day.getMonth(), day.getDate() + 1)
-  console.log(day)
+  let monthlyDays = []
+  let day = firstDateofMonth
+  while (day <= lastDateofMonth) {
+    monthlyDays.push(day)
+    day = new Date(day.getFullYear(), day.getMonth(), day.getDate() + 1)
+  }
+  let json = {
+    month: month,
+    dates: monthlyDays,
+  }
+  return json
 }
+
+for (let i = 0; i < 12; i++) {
+  months.push(calMonth(i))
+}
+
+console.log(months)
 
 export default function ScrollCalendar() {
   return (
@@ -46,26 +59,31 @@ export default function ScrollCalendar() {
 function Months() {
   const color = 'primary'
   return (
-    <div className="container-row">
-      <div className="month">
-        7월
-      </div>
-      <div className="container">
-        <div className="days">일</div>
-        <div className="days">월</div>
-        <div className="days">화</div>
-        <div className="days">수</div>
-        <div className="days">목</div>
-        <div className="days">금</div>
-        <div className="days">토</div>
-      </div>
-      <div className="container">
-        {
-          monthlyDays.map(day =>
-            <div className="date">{day.getDate()}</div>
-          )
-        }
-      </div>
+    <div>
+      {months.map(m =>
+        <div className="container-row">
+          <div className="month">
+            {m.month + 1}월
+          </div>
+          <div className="container">
+            <div className="days">일</div>
+            <div className="days">월</div>
+            <div className="days">화</div>
+            <div className="days">수</div>
+            <div className="days">목</div>
+            <div className="days">금</div>
+            <div className="days">토</div>
+          </div>
+          <div className="container">
+            {
+              m.dates.map(day =>
+                <div className="date">{day.getDate()}</div>
+              )
+            }
+          </div>
+        </div>
+      )
+      }
     </div>
   );
 }
