1dc66fcdfdbd84d9c9905fdd5eac942491b611d9
최희선 <asdfmelody@naver.com>
2022-08-01 15:17:10 +0900

    fix: change postController

---
+++ BE/phorest/src/main/java/a101/phorest/controller/PostController.java
@@ -0,0 +1,12 @@
+package a101.phorest.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/api/")
+public class PostController {
+
+}
