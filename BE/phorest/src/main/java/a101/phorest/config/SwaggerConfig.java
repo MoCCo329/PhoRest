5c6f2dfbd3e61df8a8bad2efc99c70fe7babf4cf
최희선 <asdfmelody@naver.com>
2022-08-05 10:53:44 +0900

    fix: signup phone length error

---
+++ BE/phorest/src/main/java/a101/phorest/config/SwaggerConfig.java
@@ -0,0 +1,17 @@
+package a101.phorest.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+//
+//    @Bean
+//    public Docket swagger(){
+//
+//    }
+}
