4f384b21f9bdce85c769eb2bd1e3896dcb1c3471
최희선 <asdfmelody@naver.com>
2022-08-05 11:35:06 +0900

    feat: swagger setting

--- BE/phorest/src/main/java/a101/phorest/config/SwaggerConfig.java
+++ BE/phorest/src/main/java/a101/phorest/config/SwaggerConfig.java
@@ -1,55 +1,36 @@
-//package a101.phorest.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import springfox.documentation.builders.ApiInfoBuilder;
-//import springfox.documentation.builders.PathSelectors;
-//import springfox.documentation.builders.RequestHandlerSelectors;
-//import springfox.documentation.service.ApiInfo;
-//import springfox.documentation.service.Contact;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spring.web.plugins.Docket;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
-//
-//import java.util.ArrayList;
-//
-//@Configuration
-//@EnableSwagger2
-//public class SwaggerConfig {
-//
-//    private static final String API_NAME = "PhoRest API";
-//    private static final String API_VERSION = "1.0.0";
-//    private static final String API_DESCRIPTION = "PhoRest API 명세서";
-//    @Bean
-//    public Docket apiV1() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .groupName("a101")
-//                .select()
-//                .apis(RequestHandlerSelectors.
-//                        basePackage("phorest"))
-//                .paths(PathSelectors.ant("/v1/api/**"))
-//                .build()
-//                .apiInfo(apiInfo());
-//    }
-//
-//    @Bean
-//    public Docket apiV2() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .useDefaultResponseMessages(false)
-//                .groupName("a101")
-//                .select()
-//                .apis(RequestHandlerSelectors.
-//                        basePackage("phorest"))
-//                .paths(PathSelectors.ant("/v2/api/**"))
-//                .build()
-//                .apiInfo(apiInfo());
-//    }
-//
-//    private ApiInfo apiInfo() {
-//        return new ApiInfoBuilder()
-//                .title(API_NAME)
-//                .version(API_VERSION)
-//                .description(API_DESCRIPTION)
-//                .build();
-//    }
-//}
+package a101.phorest.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.ApiSelectorBuilder;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("a101.phorest.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("API")
+                .description("API Documentation")
+                .version("1.0")
+                .build();
+    }
+}
