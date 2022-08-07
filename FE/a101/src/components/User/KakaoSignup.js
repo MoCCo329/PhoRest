96b77781fed0947aa269f319c492e1fcfdc151eb
김보경 <rawoon@kakao.com>
2022-08-07 17:02:04 +0900

    fix:원래redirect uri 로 되돌림(바꾸니까 KOE006 에러 뜸)

--- FE/a101/src/components/User/KakaoSignup.js
+++ FE/a101/src/components/User/KakaoSignup.js
@@ -8,7 +8,7 @@ export default function KakaoSignup() {
 //     });
 //   }
   const url = "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=4656da19556d6f608f3a297dd7c7b994&redirect_uri=http://localhost:3000/kakao"
-  const base = 'https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=4656da19556d6f608f3a297dd7c7b994&redirect_uri=https://phorest.site/api/user/kakao.signup'
+  const base = 'https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=4656da19556d6f608f3a297dd7c7b994&redirect_uri=https://phorest.site'
 
   function move() {
     window.location.href = base;
