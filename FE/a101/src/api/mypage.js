aad759f6e6282b385d31b0710fba20d2ea9ba1b1
김보경 <rawoon@kakao.com>
2022-08-07 20:56:36 +0900

    fix:merge conflict solved

--- FE/a101/src/api/mypage.js
+++ FE/a101/src/api/mypage.js
@@ -1,29 +1,21 @@
-<<<<<<< HEAD
-import api from './api'
+import api from "./api";
 
-const mypage = {
-  sharePost (postId) {
-    return api({
-      method: 'post',
-      url: `mypage/${btoa(postId * 73 - 37)}/share/`,
-    })
-  },
-=======
 // 내 정보, 기록들을 불러오기 위한 API
-
-import api from './api'
-
-const END_POINT = 'mypage'
-
+const END_POINT = "mypage";
 
 const mypage = {
-  userDetail (username) {
+  sharePost(postId) {
     return api({
-      method: 'get',
+      method: "post",
+      url: `mypage/${btoa(postId * 73 - 37)}/share/`,
+    });
+  },
+  userDetail(username) {
+    return api({
+      method: "get",
       url: `${END_POINT}/${username}/`,
-    })
-  }
->>>>>>> feature/fe/mypage
-}
+    });
+  },
+};
 
-export default mypage
+export default mypage;
--- FE/a101/src/store/store.js
+++ FE/a101/src/store/store.js
@@ -1,17 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
-<<<<<<< HEAD
 import { photoLike, photoRecent, frameLike, frameRecent, detailPost, detailComments, editCommentId } from './modules/community.js'
 import { token, currentUser, authError } from './modules/user.js'
-=======
-import { picPopular, picRecent, framePopular, frameRecent, pic, editCommentId, comments } from './modules/community.js'
 import { userDetail } from './modules/mypage.js'
-
->>>>>>> feature/fe/mypage
 
 export default configureStore({
   reducer: {
     // community
-<<<<<<< HEAD
     photoLike: photoLike.reducer,
     photoRecent: photoRecent.reducer,
     frameLike: frameLike.reducer,
@@ -24,17 +18,8 @@ export default configureStore({
     token: token.reducer,
     currentUser: currentUser.reducer,
     authError: authError.reducer,
-=======
-    picPopular: picPopular.reducer,
-    picRecent: picRecent.reducer,
-    framePopular: framePopular.reducer,
-    frmaeRecent: frameRecent.reducer,
-    pic: pic.reducer,
-    comments: comments.reducer,
-    editCommentId: editCommentId.reducer,
 
     // mypage
     userDetail: userDetail.reducer,
->>>>>>> feature/fe/mypage
   }
 })
