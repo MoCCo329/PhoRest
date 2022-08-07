6fdf83c75fe13b503e1eb79c9e586dad65c943be
김보경 <rawoon@kakao.com>
2022-08-07 14:35:49 +0900

    feat: 카카오 로그인 테스트중

---
+++ FE/a101/src/components/User/KakaoSignup.js
@@ -0,0 +1,26 @@
+import signup from "./KakaoApi";
+
+export default function KakaoSignup() {
+//   const END_POINT = "http://localhost:3000/kakao";
+//   function signup_kakao() {
+//     signup.signup(END_POINT).then((result) => {
+//       console.log(result.data);
+//     });
+//   }
+  const url = "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=4656da19556d6f608f3a297dd7c7b994&redirect_uri=http://localhost:3000/kakao"
+  const base = 'https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=4656da19556d6f608f3a297dd7c7b994&redirect_uri=https://phorest.site/api/user/kakao/signup'
+  const naver = 'https://naver.com'
+
+  function move() {
+    window.location.href = base;
+  }
+  return (
+    <div>
+      <button
+        onClick={move}
+      >
+        카카오로 로그인하기
+      </button>
+    </div>
+  );
+}
