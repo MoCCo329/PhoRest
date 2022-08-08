bc65c74730676083d4c3b83199b611412222a13e
최희선 <asdfmelody@naver.com>
2022-08-08 22:44:54 +0900

    fix: redirect-uri on phorest

--- BE/phorest/src/main/java/a101/phorest/service/KakaoService.java
+++ BE/phorest/src/main/java/a101/phorest/service/KakaoService.java
@@ -24,8 +24,8 @@ public class KakaoService {
 
 //    private final UserRepository userRepository;
 
-    private final String redirect_uri = "http://localhost:8399/api/user/kakao";
-    //private final String redirect_uri = "https://phorest.site/api/user/kakao";
+    //private final String redirect_uri = "http://localhost:8399/api/user/kakao";
+    private final String redirect_uri = "https://phorest.site/api/user/kakao";
 
     public List<String> getToken(String code) throws IOException {
         //인가코드로 토큰 받기
