85414c9051af524beeaa75b8518f03e228b6b0f0
김보경 <rawoon@kakao.com>
2022-08-10 03:44:59 +0900

    feat: 메인페이지 grid, icon, 로그인 헤더, 헤더 수정

--- FE/a101/src/components/User/Profile.js
+++ FE/a101/src/components/User/Profile.js
@@ -1,8 +1,10 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
-import AccountCircleTwoToneIcon from "@mui/icons-material/AccountCircleTwoTone"
 import './Profile.css'
+
+// default profile
+import defaultProfile from '../../assets/defaultProfile.png'
 
 export default function Profile(props) {
     const navigate = useNavigate()
@@ -12,9 +14,9 @@ export default function Profile(props) {
     return (
         <div className="profile" onClick={() => {navigate(`/mypage/${user.username}`)}}>
            {
-            user.profileURL ? <img src={user.profileURL} alt="" /> : <AccountCircleTwoToneIcon className="profile-header" />
+            user.profileURL ? <img className="profile-header" src={user.profileURL} alt="" /> : <img className="profile-header" src={defaultProfile} alt='default profile'/>
            }
-           {user.nickname}
+           <div className='profile-nickname'>{user.nickname}</div>
         </div>
     )
   }
