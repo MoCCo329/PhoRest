3fafd6c681f7b13343c4990c8ed8894d26075fac
junsu <xofkdqkqh@naver.com>
2022-08-11 10:13:31 +0900

    feat: s3 upload 확장자 체크 추가

--- BE/phorest/src/main/java/a101/phorest/S3Uploader.java
+++ BE/phorest/src/main/java/a101/phorest/S3Uploader.java
@@ -5,6 +5,7 @@ import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
@@ -14,6 +15,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.util.Locale;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -28,7 +30,7 @@ public class S3Uploader {
 
     public String uploadFiles(MultipartFile multipartFile, String dirName) throws IOException {
         File uploadFile = convert(multipartFile)  // 파일 변환할 수 없으면 에러
-                .orElseThrow(() -> new IllegalArgumentException("error: MultipartFile -> File convert fail"));
+                .orElseThrow(() -> new IllegalArgumentException("error: 잘못된 확장자, 경로이거나 경로에 같은 이름의 파일이 있습니다."));
         return upload(uploadFile, dirName);
     }
 
@@ -72,6 +74,17 @@ public class S3Uploader {
     // 로컬에 파일 업로드 하기
     private Optional<File> convert(MultipartFile file) throws IOException {
         File convertFile = new File(System.getProperty("user.dir") + "/imagefiles/" + file.getOriginalFilename());
+        final String[] PERMISSION_FILE_EXT_ARR = {"GIF", "JPEG", "JPG", "PNG", "BMP", "MP4"};
+        boolean flag = false;
+        String ext = FilenameUtils.getExtension(convertFile.getName()).toUpperCase();
+        for(String allowedExt : PERMISSION_FILE_EXT_ARR){
+            if(allowedExt.equals(ext)) {
+                flag = true;
+                break;
+            }
+        }
+        if(!flag)
+            return Optional.empty();
         if (convertFile.createNewFile()) { // 바로 위에서 지정한 경로에 File이 생성됨 (경로가 잘못되었다면 생성 불가능)
             try (FileOutputStream fos = new FileOutputStream(convertFile)) { // FileOutputStream 데이터를 파일에 바이트 스트림으로 저장하기 위함
                 fos.write(file.getBytes());
