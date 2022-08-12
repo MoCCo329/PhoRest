8ea1f3deb45b06524b85f537ae93bf5a7cb26fce
김도현 <kd8317@gmail.com>
2022-08-12 14:57:50 +0900

    feat: Finish follower-component

---
+++ FE/a101/src/components/MyPage/FollowerList.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import Profile from '../User/Profile'
+
+import user from './../../api/user'
+
+export default function FollowerList() {  
+  const [followerList, setFollowerList] = useState('')
+  const userDetail = useSelector(state => state.userDetail)
+
+  useEffect(() => {
+    user.followerList(userDetail.username)
+    .then(result => {
+      setFollowerList(result.data)
+    })
+  }, [])
+
+
+  return (
+    <div>
+        {
+          followerList && followerList.length ?
+          followerList.map(user => <Profile user={user} key={user.username}/>) :
+          <p>팔로워가 없습니다</p>
+        }
+    </div>
+  )
+}
--- FE/a101/src/components/MyPage/FollowingList.js
+++ FE/a101/src/components/MyPage/FollowingList.js
@@ -16,9 +16,9 @@ export default function FollowingList() {
   return (
     <div>
         {
-          followingList ?
+          followingList && followingList.length ?
           followingList.map(user => <Profile user={user} key={user.username}/>) :
-          null
+          <p>팔로우중인 사람이 없습니다</p>
         }
     </div>
   )
