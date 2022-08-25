76af9bfdd78a94860411afcc240f70ac8e23152c
김보경 <rawoon@kakao.com>
2022-08-25 09:24:15 +0900

    fix: using 404 page

--- dev/FE/a101/src/pages/NotFound404.js
+++ dev/FE/a101/src/pages/NotFound404.js
@@ -23,7 +23,10 @@ export default function NotFound404() {
             navigate(`/download/${btoa((postForKakao) * 73 - 37)}`)
             return dispatch(setPostForKakao(''))
         }
-        // } else  if (location.pathname==='/login') {
+        else if (location.pathname === '/kakao') {
+            navigate(-2)
+        }
+        // else  if (location.pathname==='/login') {
         //     navigate(-1)
         // }
     }, [currentUser, location])
